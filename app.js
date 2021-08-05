d7c19834eaba97052fb96b28eaf87c39ef8b5cbc
hnsblnbrg <hansibrahimasi@gmail.com>
2021-08-05 09:32:25 +0200

    Target the first 10 entries only

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ async function getPriceFeed() {
       "#__next > div > div.main-content > div.sc-57oli2-0.dEqHl.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody > tr";
 
     $(elementSelector).each((parentIndex, parentElement) => {
-      console.log(parentElement);
+      //Get the top 10
+      if (parentIndex <= 9) {
+        $(parentElement)
+          .children()
+          .each((childIndex, childElement) => {
+            console.log($(childElement).text());
+          });
+      }
     });
   } catch (err) {
     console.error(err);
